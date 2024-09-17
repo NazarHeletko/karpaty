d94aeae1b33a0fe4a9495873623fbb3fe2ef39fa
Nazar <37541828+NazarHeletko@users.noreply.github.com>
2024-09-17 15:44:29 +0300

    add some metadata

--- src/app/realty/page.tsx
+++ src/app/realty/page.tsx
@@ -1,6 +1,14 @@
 import FilterRealty from "@/components/Page/RealtyPage/FilterRealty/FilterRealty";
 import RealtyItems from "@/components/Page/RealtyPage/RealtyItems/RealtyItems";
 import RealtyPage from "@/components/Page/RealtyPage/RealtyPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Житло у Карпатах | будиночки, готелі, котеджі, відпочинкові комплекси| Мої Карпати",
+  description:
+    "Тут знайдете найкращі варіанти проживання у Карпатах: затишні будиночки, котеджі та готелі на будь-який смак. Комфортний відпочинок серед мальовничих Карпат. Відпочинок у Карпатах стане незабутнім.",
+};
 
 export default function Realty() {
   return (
