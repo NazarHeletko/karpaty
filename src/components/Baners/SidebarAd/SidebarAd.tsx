7be7c04b2799e51fd7b78eae7b5eab7d4f7a1b41
Nazar <37541828+NazarHeletko@users.noreply.github.com>
2024-09-29 20:14:20 +0300

    search city addd12d

--- src/components/Baners/SidebarAd/SidebarAd.tsx
+++ src/components/Baners/SidebarAd/SidebarAd.tsx
@@ -5,13 +5,25 @@ type Props = {
   src: string;
   alt: string;
   title: string;
+  location: string;
 };
 
-export default function ImageArticleRight({ src, alt, title }: Props) {
+export default function ImageArticleRight({ src, alt, title, location }: Props) {
   return (
     <div className={styles["sidebar-ad"]}>
       <Image src={src} fill alt={alt} style={{ objectFit: "cover" }} />
-      <span>{title}</span>
+      <div className={styles.description}>
+        <span>{title}</span>
+        <div className={styles.location}>
+          <Image
+            src="/icons/location-icon.svg"
+            height={14}
+            width={14}
+            alt="location"
+          />
+          <span className={styles["location-title"]}>Верховина</span>
+        </div>
+      </div>
     </div>
   );
 }
