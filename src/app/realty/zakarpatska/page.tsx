bccaf9666c15a7f2138a6dcef6f4599fc732557f
Nazar <37541828+NazarHeletko@users.noreply.github.com>
2024-10-02 20:33:41 +0300

    dddwd

--- src/app/realty/zakarpatska/page.tsx
+++ src/app/realty/zakarpatska/page.tsx
@@ -4,42 +4,43 @@ import RealtyPage from "@/components/Page/RealtyPage/RealtyPage";
 import { Metadata } from "next";
 import realty from "@/data/realty.json";
 
-export const metadata: Metadata = {
-  title:
-    "Житло у Карпатах | будиночки, готелі, котеджі, відпочинкові комплекси| Мої Карпати",
-  description:
-    "Тут знайдете найкращі варіанти проживання у Карпатах: затишні будиночки, котеджі та готелі на будь-який смак. Комфортний відпочинок серед мальовничих Карпат. Відпочинок у Карпатах стане незабутнім.",
-};
+// export const metadata: Metadata = {
+//   title:
+//     "Житло у Карпатах | будиночки, готелі, котеджі, відпочинкові комплекси| Мої Карпати",
+//   description:
+//     "Тут знайдете найкращі варіанти проживання у Карпатах: затишні будиночки, котеджі та готелі на будь-який смак. Комфортний відпочинок серед мальовничих Карпат. Відпочинок у Карпатах стане незабутнім.",
+// };
 
-type DataType = {
-  id: number;
-  oblast: string;
-  raion: string;
-  city: string;
-  name: string;
-  price: string;
-  "people-count": number;
-  "title-image": string;
-};
+// type DataType = {
+//   id: number;
+//   oblast: string;
+//   raion: string;
+//   city: string;
+//   name: string;
+//   price: string;
+//   "people-count": number;
+//   "title-image": string;
+// };
 
-export const revalidate = 0;
+// export const revalidate = 0;
 
 export default async function Realty() {
-  function shuffleArray(array: DataType[]) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-      [array[i], array[j]] = [array[j], array[i]]; // swap elements
-    }
-    return array;
-  }
+//   function shuffleArray(array: DataType[]) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+//       [array[i], array[j]] = [array[j], array[i]]; // swap elements
+//     }
+//     return array;
+//   }
  
-  const randomizeRealtyArray = shuffleArray(realty);
+//   const randomizeRealtyArray = shuffleArray(realty);
 
 
   return (
-    <RealtyPage>
-      <FilterRealty building={""} oblast={"/zakarpatska"} raion={""} city={""}/>
-      <RealtyItems realtyData={randomizeRealtyArray} />
-    </RealtyPage>
+    // <RealtyPage>
+    //   <FilterRealty building={""} oblast={"/zakarpatska"} raion={""} city={""}/>
+    //   <RealtyItems realtyData={randomizeRealtyArray} />
+    // </RealtyPage>
+    <p>cdx</p>
   );
 }
