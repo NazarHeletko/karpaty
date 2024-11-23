92825f406b0821b377edfa2ac8f6cc22702c3296
Nazar <37541828+NazarHeletko@users.noreply.github.com>
2024-11-24 00:09:29 +0200

    ouihвувddуo9u

---
+++ src/app/realty/zakarpatska/rakhiv-raion/yasynia/cottage/page.tsx
@@ -0,0 +1,48 @@
+import FilterRealty from "@/components/Page/RealtyPage/FilterRealty/FilterRealty";
+import RealtyItems from "@/components/Page/RealtyPage/RealtyItems/RealtyItems";
+import RealtyPage from "@/components/Page/RealtyPage/RealtyPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Котеджі у Ясіня | Мої Карпати",
+};
+
+export const revalidate = 0;
+
+export default async function Realty() {
+  return (
+    <RealtyPage>
+      <FilterRealty
+        oblast={"/zakarpatska"}
+        raion={"/rakhiv-raion"}
+        city={"/yasynia"}
+        building={"/cottage"}
+      />
+      <RealtyItems
+        firstFilter={[
+          { title: "Котеджі у Ясіня" },
+          { city: "/yasynia", type: "cottage" },
+        ]}
+        secondFilter={[{ title: "Все житло у Ясіня" }, { city: "/yasynia" }]}
+        thirdFilter={[
+          { title: "Житло у Рахівському районі" },
+          { raion: "/rakhiv-raion" },
+        ]}
+        forthFilter={[
+          { title: "У Закарпатській області" },
+          { oblast: "/zakarpatska" },
+        ]}
+        fifthFilter={[{ title: "Все житло в Карпатах" }, { place: "all" }]}
+      />
+      <p
+        style={{
+          color: "#2e2e2e",
+          textAlign: "justify",
+          fontSize: "14px",
+          fontWeight: "300",
+          lineHeight: "21px",
+        }}
+      ></p>
+    </RealtyPage>
+  );
+}
