224dc8f8d3c0e58451fa7c27541a18339b425aec
Nazar <37541828+NazarHeletko@users.noreply.github.com>
2024-12-12 20:42:00 +0200

    defaadfewfddew

---
+++ src/app/realty/zakarpatska/tyachiv-raion/tereshva/cottage/page.tsx
@@ -0,0 +1,48 @@
+import FilterRealty from "@/components/Page/RealtyPage/FilterRealty/FilterRealty";
+import RealtyItems from "@/components/Page/RealtyPage/RealtyItems/RealtyItems";
+import RealtyPage from "@/components/Page/RealtyPage/RealtyPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Котеджі в Тересва | Мої Карпати",
+};
+
+export const revalidate = 0;
+
+export default async function Realty() {
+  return (
+    <RealtyPage>
+      <FilterRealty
+        oblast={"/zakarpatska"}
+        raion={"/tyachiv-raion"}
+        city={"/tereshva"}
+        building={"/cottage"}
+      />
+      <RealtyItems
+        firstFilter={[
+          { title: "Котеджі в Тересва" },
+          { city: "/tereshva", type: "cottage" },
+        ]}
+        secondFilter={[{ title: "Все житло в Тересва" }, { city: "/tereshva" }]}
+        thirdFilter={[
+          { title: "Житло у Тячівському районі" },
+          { raion: "/tyachiv-raion" },
+        ]}
+        forthFilter={[
+          { title: "Житло у Закарпатській області" },
+          { oblast: "/zakarpatska" },
+        ]}
+        fifthFilter={[{ title: "Все житло в Карпатах" }, { place: "all" }]}
+      />
+      <p
+        style={{
+          color: "#2e2e2e",
+          textAlign: "justify",
+          fontSize: "14px",
+          fontWeight: "300",
+          lineHeight: "21px",
+        }}
+      ></p>
+    </RealtyPage>
+  );
+}
